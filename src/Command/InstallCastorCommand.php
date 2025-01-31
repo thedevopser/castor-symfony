f50b8c7e9ca8a18e7007b1a13880d47c950a030e
TheDevOpser <sylvain.anglade@gmail.com>
2025-01-31 23:26:28 +0100

    fix(command): remove unused AsCommand attribute from InstallCastorCommand

--- src/Command/InstallCastorCommand.php
+++ src/Command/InstallCastorCommand.php
@@ -2,7 +2,6 @@
 
 namespace TheDevOpser\CastorBundle\Command;
 
-use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
