5c348e0161a3bef6100d15bd4aec1eb5c26fdae0
TheDevOpser <sylvain.anglade@gmail.com>
2025-02-02 10:11:06 +0100

    feat(greet): add greeting function to prompt user for name and display a message

--- src/Data/castorPersonal.php
+++ src/Data/castorPersonal.php
@@ -1,1 +1,14 @@
-<?php // Test personal file
+<?php
+
+use Castor\Attribute\AsTask;
+use Castor\Context;
+
+use function Castor\io;
+use function Castor\run;
+
+#[AsTask('Greeting')]
+function greet(): void
+{
+    $name = io()->ask('What is your name?');
+    io()->write("Hello, $name!");
+}
